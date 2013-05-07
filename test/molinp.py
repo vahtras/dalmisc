34f696639ce8833d2a0315611db37e7fa24f505d
Olav Vahtras <vahtras@kth.se>
2013-05-07 15:24:50 +0200

    mol file

---
+++ test/molinp.py
@@ -0,0 +1,32 @@
+ch2 = """ATOMBASIS
+STO-3G
+------
+    2   0 Z
+        6.    1 Basis=STO-3G
+C1      0.0006122714    0.0000000000    0.0000000000   
+        1.    2 Basis=STO-3G
+H1      1.5162556382   -1.3708721537    0.0000000000
+H2     -0.7584339548    0.6854360769    1.7695110698
+"""
+
+h2o = """ATOMBASIS
+Energy calculation without symmetry
+Basis set specified with ATOMBASIS
+    2    0
+        8.    1      Basis=cc-pVDZ
+O     0.0 -0.2249058930 0.0
+        1.    2      Bas=cc-pVDZ
+H1    1.45235 0.899623 0.0
+H2   -1.45235 0.899623 0.0
+"""
+
+h2op = """ATOMBASIS
+Energy calculation without symmetry
+Basis set specified with ATOMBASIS
+    2  1 0
+        8.    1      Basis=cc-pVDZ
+O     0.0 -0.2249058930 0.0
+        1.    2      Bas=cc-pVDZ
+H1    1.45235 0.899623 0.0
+H2   -1.45235 0.899623 0.0
+"""
