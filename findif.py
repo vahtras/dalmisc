5211c52320d83869e1e43b003684ed534cf2512e
Olav Vahtras <vahtras@kth.se>
2013-06-03 17:11:25 +0200

    A more general class for all response orders

--- findif.py
+++ findif.py
@@ -293,4 +293,130 @@ class CubResp:
         return result
         
 
+class RspCalc:
+    """Execute dalton LR"""
+    def __init__(self, *args, **kwargs):
+        self.parallel = kwargs.get('parallel', True)
+
+        self.wf = kwargs.get('wf', 'HF')
+        self.field = kwargs.get('field', None)
+        self.delta = kwargs.get('delta', 0)
+
+        self.ops = args
+        self.triplet = kwargs.get('triplet', False)
+        self.aux = kwargs.get('aux', '#')
+
+        self.dal = kwargs.get('dal', self.wf)
+        self.mol = kwargs.get('mol', None)
+
+    def exe(self, delta=None):
+
+        global ncpu
+        #Wave function
+        if self.field and delta:
+            ff = "*HAMILTON\n.FIELD\n%f\n%s"%(delta, self.field)
+        else:
+            ff = "###"
+
+
+        wavinp = """**WAVE FUNCTIONS
+.%s
+*SCF INPUT
+.NOQCSCF
+.THRESHOLD
+1e-10
+%s""" % (self.wf, ff)
+
+        #Response
+        if self.triplet:
+            trpflg = ".TRPFLG"
+        else:
+            trpflg = "#"
+
+        rsp_order = len(self.ops)
+
+        if (rsp_order == 1):
+            A, = self.ops
+            rspinp = """**RESPONSE
+%s
+.PROPAV
+%s""" % (trpflg, A)
+
+        elif (rsp_order == 2):
+            A, B = self.ops
+            rspinp = """**RESPONSE
+%s
+*LINEAR
+.THCLR
+1e-9
+.PROPRT
+%s
+.PROPRT
+%s
+%s """ % (trpflg, A, B, self.aux)
+
+        elif rsp_order == 3:
+            if not self.parallel: ncpu = 1
+            A, B, C = self.ops
+            rspinp = """**RESPONSE
+%s
+*QUADRATIC
+.THCLR
+1e-9
+.APROP 
+%s
+.BPROP
+%s
+.CPROP
+%s
+%s """ % (trpflg, A, B, C, self.aux)
+
+        else:
+            raise RuntimeError("Response order %d not implemented" % rsp_order)
+
+        dalinp = """**DALTON INPUT
+.RUN RESPONSE
+%s
+%s
+**END OF DALTON
+""" % (wavinp, rspinp)
+
+        dal = self.dal.split('\n')[-1].replace(' ','_').replace('/','_')
+        dalfile = open(dal + ".dal", 'w')
+        dalfile.write(dalinp)
+        dalfile.close()
+    
+        molfile = open(dal + ".mol", 'w')
+        molfile.write(self.mol)
+        molfile.close()
+
+        cmd = "dalton -N %d -d -t /tmp/ExpVal_%s %s > log 2>&1 " % (ncpu, dal, dal)
+        os.system(cmd)
+
+        result = None
+        A  = A.split()[0]
+        for line in open(dal + ".out"):
+            if rsp_order == 1:
+                if "total" in line and A in line:
+                    data = line.split(':')[1].replace('D', 'E')
+                    result = float(data)
+                    break
+            elif rsp_order == 2:
+                if "@" in line and A in line and B in line:
+                    data = line.split('=')[1].replace('D', 'E')
+                    result = -float(data)
+                    break
+            elif rsp_order == 3:
+                if "@omega" in line:
+                    data = line.split()[-1]
+                    result = float(data)
+                    break
+            else:
+                raise RuntimeError("Response order %d not implemented" % rsp_order)
+
+        if result is None or math.isnan(result): 
+            #import pdb; pdb.set_trace()
+            raise ValueError
+        return result
+
 
--- test/test_findif.py
+++ test/test_findif.py
@@ -25,46 +25,46 @@ def teardown():
     pass
 
 def test_EV_HF():
-    ev = ExpVal('XDIPLEN', mol=inp["ch2+"])
+    ev = RspCalc('XDIPLEN', mol=inp["ch2+"])
     res = ev.exe()
     assert_(res, 3.1348738989E-03)
 
 def test_EV_DFT():
-    ev = ExpVal('XDIPLEN', wf='DFT\nB3LYP', dal='b3lyp', mol=inp["ch2+"])
+    ev = RspCalc('XDIPLEN', wf='DFT\nB3LYP', dal='b3lyp', mol=inp["ch2+"])
     res = ev.exe()
     assert_(res, 3.18476890e-03)
 
 def test_LR_HF(): 
-    lr = LinResp('XANGMOM', 'X1SPNORB', mol=inp["ch2+"])
+    lr = RspCalc('XANGMOM', 'X1SPNORB', mol=inp["ch2+"])
     res = lr.exe()
     assert_(res, -0.000131051802829)
 
 def test_LR_DFT(): 
-    lr = LinResp('XANGMOM', 'X1SPNORB', wf='DFT\nLDA', dal='lda', mol=inp["ch2+"])
+    lr = RspCalc('XANGMOM', 'X1SPNORB', wf='DFT\nLDA', dal='lda', mol=inp["ch2+"])
     res = lr.exe()
     assert_(res, -1.770146614464e-04)
 
 def test_lr_HFx():
-    lr = LinResp('XANGMOM', 'X1SPNORB', mol=inp["ch2+"], field='XDIPLEN', delta=0.0001)
+    lr = RspCalc('XANGMOM', 'X1SPNORB', mol=inp["ch2+"], field='XDIPLEN', delta=0.0001)
     fd = FinDif(lr)
     res = fd.first()
     assert_(res, 0.00057501)
 
 def test_QR_HF(): 
-    qr = QuadResp('XANGMOM', 'X1SPNORB', 'XDIPLEN', wf="DFT\nGGAKEY HF=1", mol=inp["ch2+"], parallel=False)
+    qr = RspCalc('XANGMOM', 'X1SPNORB', 'XDIPLEN', wf="DFT\nGGAKEY HF=1", mol=inp["ch2+"], parallel=False)
     res = qr.exe()
     assert_(res, 0.00057501)
 
 def test_LRx_QR():
         wf="HF"
         lr = FinDif(
-            LinResp('XANGMOM', 'X1SPNORB', wf=wf, mol=inp["ch2+"], field='XDIPLEN', delta=0.0001)
+            RspCalc('XANGMOM', 'X1SPNORB', wf=wf, mol=inp["ch2+"], field='XDIPLEN', delta=0.0001)
             ).first()
-        qr = QuadResp('XANGMOM', 'X1SPNORB', 'XDIPLEN', mol=inp["ch2+"], wf=wf, parallel=False).exe()
+        qr = RspCalc('XANGMOM', 'X1SPNORB', 'XDIPLEN', mol=inp["ch2+"], wf=wf, parallel=False).exe()
         assert_(lr, qr)
     
 
 if __name__ == "__main__":
     setup()
-    test_LR_HF()
+    test_EV_HF()
     teardown()
