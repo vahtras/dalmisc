2ebe1282787d459ae4e3a59dde15f898091285d1
Olav Vahtras <vahtras@kth.se>
2014-05-09 16:52:13 +0200

    Make setup file

---
+++ xyz2mol/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    scripts=["xyz2mol.py"],
+    name="xyz2mol",
+    version="1.0",
+    url="None",
+    author="Olav Vahtras",
+    author_email="olav.vahtras@gmail.com",
+    )
+
