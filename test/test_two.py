3b94c0eb3c433df7986c120d92945769af4f165a
Olav Vahtras <vahtras@kth.se>
2015-01-23 17:34:41 +0100

    Use unittest in test_two.py
    Also generate integrals on fly instead of keeping them in repo

--- test/test_two.py
+++ test/test_two.py
@@ -1,33 +1,55 @@
+import  unittest
 import os
 import numpy as np
 from daltools import one, dens
-from dalmisc import two
+from .. import two
+
+class TwoTest(unittest.TestCase):
+
+    def setUp(self):
+        n, e = os.path.splitext(__file__)
+        suppdir = n + ".d"
+        self.aotwoint = os.path.join(suppdir, "AOTWOINT")
+
+    def test_basinfo(self):
+        info = two.info(self.aotwoint)
+        self.assertEqual(info["nsym"], 1)
+        np.testing.assert_equal(info["nbas"], [7,0,0,0,0,0,0,0])
+        self.assertEqual(info["lbuf"], 600)
+        self.assertEqual(info["nibuf"], 1)
+        self.assertEqual(info["nbits"], 8)
 
 
-def setup():
-    global suppdir
-    n, e = os.path.splitext(__file__)
-    suppdir = n + ".d"
-
-    global h1, S, EN, aooneint, aotwoint, na, nb
-    aotwoint = os.path.join(suppdir, "AOTWOINT")
-
-def assert_(this, ref):
-    print this
-    print ref
-    assert np.allclose(this, ref)
-
-def test_basinfo():
-    info = two.info(aotwoint)
-    assert_(info["nsym"], 1)
-    assert_(info["nbas"], [7,0,0,0,0,0,0,0])
-    assert_(info["lbuf"], 600)
-    assert_(info["nibuf"], 1)
-    assert_(info["nbits"], 8)
+    def test_first_integral(self):
+        for ig, g in two.list_integrals(self.aotwoint):
+            break
+        self.assertAlmostEqual(g, 4.78506540471)
+        np.testing.assert_equal(ig, [1,1,1,1])
 
 
-def test_first_integral():
-    for ig, g in two.list_integrals(aotwoint):
-        break
-    assert_(g, 4.78506540471)
-    assert_(ig, [1,1,1,1])
+class TestBase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        root, ext = n, e = os.path.splitext(__file__)
+        cls.base_dir = root + ".d"
+        
+class TestH2O(TestBase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestH2O, cls).setUpClass()
+        cls.subdir = "H2O"
+        cls.dal = "hf"
+        cls.mol = "H2O_ccpVDZ"
+        cls.filename = "%s_%s.AOTWOINT" % (cls.dal, cls.mol)
+        cls.tmpdir = os.path.join(cls.base_dir, cls.subdir)
+        cls.aotwoint = os.path.join(cls.tmpdir, cls.filename)
+        if not os.path.exists(cls.aotwoint):
+            os.chdir(cls.tmpdir)
+            os.system('dalton -get AOTWOINT hf H2O_ccpVDZ')
+
+    def test_number_of_integrals(self):
+        self.assertEqual(len(list(two.list_integrals(self.aotwoint))), 11412)
+
+    
