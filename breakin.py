25243d1a73837b5fa2c13c192eaa3774995b5a6a
Olav Vahtras <vahtras@kth.se>
2018-08-21 16:10:05 +0200

    remove break snippet

--- breakin.py
+++
@@ -1,7 +0,0 @@
-def breakin(fu):
-    """ Decorator that loads the debugger in the beginning of the decorated function"""
-    def wrap(*args, **kwargs):
-        import pdb; pdb.set_trace()
-        result = fu(*args, **kwargs)
-        return result
-    return wrap
