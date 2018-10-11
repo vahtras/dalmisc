5fc804e8c4e7ab60c469fff602ee3ff87639a3af
Olav Vahtras <vahtras@kth.se>
2018-10-11 17:08:19 +0200

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="dalmisc",
+    author="Olav Vahtras",
+    author_email="olav.vahtras@gmail.com",
+    install_requires=["blocked-matrix-utils"],
+)
