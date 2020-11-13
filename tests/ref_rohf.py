f9cc8b167daa6bb2593f4641becae9e1a0c80605
Olav Vahtras <vahtras@kth.se>
2020-11-13 14:37:37 +0100

    dito

---
+++ tests/ref_rohf.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+C1 = np.loadtxt('tests/test_rohf.d/C1.ref')
+Ca = np.loadtxt('tests/test_rohf.d/Ca.ref')
+Cb = np.loadtxt('tests/test_rohf.d/Cb.ref')
+Da = np.loadtxt('tests/test_rohf.d/Da.ref')
+Db = np.loadtxt('tests/test_rohf.d/Db.ref')
