2e344132eaac0d163cdb41a5737baca8e875fc49
Olav Vahtras <vahtras@kth.se>
2020-12-15 11:38:29 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup
 
 setup(
     name="dalmisc",
+    version="0.0.1",
     author="Olav Vahtras",
     author_email="olav.vahtras@gmail.com",
+    packages=["dalmisc"],
     install_requires=["blocked-matrix-utils", "daltools", "two-electron"],
 )
